54067179ef2b377a06a9a49740c7bcb8e5a1ce6c
KevJord76 <kevjord76@gmail.com>
2022-11-15 20:32:25 +0000

    Add footer: lotto game rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,9 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 /**
- * Sets the text of the generate button to the lottery game chosen and sets the button colour as follows:
- * green for euromillions, red for lotto and blue for dailymillions 
+ * Sets the text of the generate numbers button to the lottery game chosen and sets the button colour  
+ * as follows: green for euromillions, red for lotto and blue for dailymillions
+ * Sets the minimun number of lines per game, e.g. the lotto game requires a minimum of 2 lines  
  */
 function lotteryGame(lotteryType) {
 
@@ -28,10 +29,13 @@ function lotteryGame(lotteryType) {
     // Set the generate numbers button colour
     if (lotteryType === "euromillions") {
         document.getElementById("generate-numbers").style.color = '#008000'; // green
+        document.getElementById("number-lines").value = "1";
     } else if (lotteryType === "lotto") {
         document.getElementById("generate-numbers").style.color = '#FF0000'; // red
+        document.getElementById("number-lines").value = "2"; // mimimum of 2 lines
     } else if (lotteryType === "dailymillions") {
         document.getElementById("generate-numbers").style.color = '#0000FF'; // blue
+        document.getElementById("number-lines").value = "1";
     } else {
         alert(`Unknown lottery game type: ${lotteryType}`)
         throw `Unknown lottery game type: ${lotteryType}. Aborting!`;
