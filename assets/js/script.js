f58f80b1046cb0c3e47f2cd23d9ee9877312d332
KevJord76 <kevjord76@gmail.com>
2022-11-17 20:46:46 +0000

    Add generateNumbers function to create random numbers array

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,13 +43,14 @@ function lotteryGame(lotteryType) {
 }
 
 /**
- * Check which lottery game the user has chosen, then define the game rules:  
- * Total number of lottery balls in the draw, number to be chosen and are lucky stars included    
-  */
+ * Check which lottery game the user has chosen, then define the 
+ * game rules: Total number of lottery balls in the draw, 
+ * number of balls to be chosen and are lucky stars included    
+ */
 function checkGame() {
 
     // Store the text of the chosen lottery game
-    let checkGame = document.getElementById("generate").innerText;
+    let chosenGame = document.getElementById("generate").innerText;
     
     // Default to Euromillions: 50 numbers in total, 5 numbers to be generated, lucky stars included
     let totalNumbers = 50;
@@ -57,37 +58,54 @@ function checkGame() {
     let luckyStars = true;
 
     // Check what the chosen lottery game is
-    if (checkGame.includes("EUROMILLIONS")) {
+    if (chosenGame.includes("EUROMILLIONS")) {
         totalNumbers = 50;
         chosenNumbers = 5;
         luckyStars = true;
-    } else if (checkGame.includes("LOTTO")) {
+    } else if (chosenGame.includes("LOTTO")) {
         totalNumbers = 47;
         chosenNumbers = 6;
         luckyStars = false;
-    } else if (checkGame.includes("DAILYMILLIONS")) {
+    } else if (chosenGame.includes("DAILYMILLIONS")) {
         totalNumbers = 39;
         chosenNumbers = 6;
         luckyStars = false;
     } else {
-        alert("Unknown lottery game type");
-        throw "Unknown lottery game type";
+        alert("Unknown lottery game chosen");
+        throw "Unknown lottery game chosen. Aborting!";
     }
 
     // Generte the random lottery numbers
-    generateNumbers(totalNumbers, chosenNumbers, luckyStars);    
-
+    generateNumbers(totalNumbers, chosenNumbers, luckyStars);
 }
 
 /**
- * This function generates the random lottery numbers
+ * This function generates the random lottery numbers based
+ * on the total number of lottery balls in the draw, 
+ * number of balls to be chosen and are lucky stars included 
  */
 function generateNumbers(totalNumbers, chosenNumbers, luckyStars) {
 
-    // Store the number of lines and the number of members
+    // Store the number of lines chosen
     let numLines = parseInt(document.getElementById("num-lines").value);
-    let numMembers = parseInt(document.getElementById("num-members").value);
+    
+    // Array of lottery game random numbers
+    let randomNumbers = [];
 
-    alert(totalNumbers+" "+chosenNumbers+" "+luckyStars);
+    // Continue until there is a full set of numbers for the game
+    do {
+        // Creates a random number between 1 and total number in the lottery game
+        let num = Math.floor(Math.random() * totalNumbers) + 1;
 
+        // Only store unique numbers
+        if (!randomNumbers.includes(num)) {
+            // Add to the array
+            randomNumbers.push(num);
+        }
+            
+    } while (randomNumbers.length <  chosenNumbers);
+        
+    // numeric sort function discovered on www.w3schools.com 
+    randomNumbers.sort(function(a, b){return a-b});
+    alert(randomNumbers);
 }     
